95beea437b293f27491f419f64983c0b429fc7f6
wablerfam <wablerfam@gmail.com>
2017-11-26 20:20:35 +0900

    feat: -v check version option (#4)

--- baku.go
+++ baku.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
-func main() {
-	Logger("info", "baku.main", "start")
+var version string
 
+func chechVerrsion() {
+	fmt.Println(version)	
+}
+
+func main() {
 	var (
+		parseVersion = flag.Bool("v", false, "check version")
 		parseFile = flag.String("c", "default", "specify config file")
 	)
 
 	flag.Parse()
 
+	if *parseVersion == true {
+		chechVerrsion()
+		return
+	}
+
+	Logger("info", "baku.main", "start")
+	
 	if *parseFile == "default" {
 		Logger("fatal", "baku.main", "not set -c [config_file]")
 	}
